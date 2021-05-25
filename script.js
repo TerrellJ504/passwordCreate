331e8d67e38644c5fad67c599cc9a9cdcca40bc4
Terrell Morris <terrellmorris@terrells-mbp.attlocal.net>
2021-05-25 19:22:26 -0400

    html and js changes

--- script.js
+++ script.js
@@ -15,48 +15,64 @@ generateBtn.addEventListener("click", writePassword);
 
 
 function generatePassword() {
+  var choice_set = ""
   alert("Generating password...")
   // Take input from user on how many charaters they want their password to be.
   var total_chars = prompt("How many charaters do you want?")
   // Turn the string I got from the user into a number
   var total_chars_number = parseInt(total_chars)
   // If the length of charaters is less than 8, tell the user the length is too short
-  if (total_chars_number > 0){
+  if (total_chars_number > 0) {
     alert(total_chars_number);
-}
+  }
   // If the length of charaters is greater than 128, tell them the input is too long
-  (textboxentered.value>128)
-  {
+  if (total_chars_number > 128) {
     alert("Please enter text upto 128 Charecters");
-  }  
+  }
   // Take input from user on if they want capital letters
   var need_capitals = window.confirm("if they want capital letters: ");
-  if(need_capitals){
+  if (need_capitals) {
     alert("You chose to add capital letters")
-  }else{
+    choice_set += "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+  } else {
     alert("You chose not to use capital letters.")
   }
   // Take input from user on if they want symbols
   var want_symbols = window.confirm("if they want symbols: ");
-  if(want_symbols){
+  if (want_symbols) {
     alert("You chose to add symbols")
-  }else{
+    choice_set += "!@#$%^&*()_+"
+  } else {
     alert("You chose not to use symbols.")
   }
   // Take input from user on if they want numbers
   var want_numbers = window.confirm("if they want numbers: ");
-  if(want_numbers){
+  if (want_numbers) {
     alert("You chose to add numbers")
-  }else{
+    choice_set += "1234567890"
+  } else {
     alert("You chose not to use numbers.")
   }
   // Take input from user on lower case letters
   var want_lowercase = window.confirm("if they want capital letters: ");
-  if(want_lowercase){
+  if (want_lowercase) {
     alert("You chose to add lower case")
-  }else{
+    choice_set += "abcdefghijklmnopqrstuvwxyz"
+  } else {
     alert("You chose not to use lower case.")
   }
+  let out_password = ""
+  // Choose random letters out of choice set
+  for(var i = 0; i < total_chars_number; i++){
+    out_password += choice_set.charAt(Math.floor(Math.random() * choice_set.length))
+  }
 
-  var String
+
+
+
+
+
+  return out_password
+
+
 }
